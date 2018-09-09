1a1b214dcd506d106f83500317311c73dfbd70c0
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-09-09 23:17:14 +0800

    路由配置

---
+++ src/components/GamePages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class GamePages extends React.Component{
+    render(){
+        return(<div>
+            <div className={{width:'100px',height:'100px','background':'red'}}></div>
+        </div>)
+    }
+}
+
+export default GamePages;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore,applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { logger } from 'redux-logger';
 import rootReducers from './reducers/index';
+import GamePage from './components/GamePages'
+import { BrowserRouter as Router, Route,NavLink } from 'react-router-dom'
+
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -15,7 +18,18 @@ const store = createStore(rootReducers,
 
 ReactDOM.render(
     <Provider store ={ store }>
-        <App/>
+        <Router>
+            <div className='ui container'>
+              <div className='ui three item menu'>
+                <NavLink exact className='item' to='/'>Home</NavLink>
+                <NavLink exact className='item' to='/games'>Games</NavLink>
+                <NavLink exact className='item' to='/'>Add new Games</NavLink>
+              </div>
+              <Route path='/' component={ App }/>
+              <Route path='/games' component={ GamePage }/>
+            </div>
+        </Router>
+       
     </Provider>,
  document.getElementById('root'));
 registerServiceWorker();
