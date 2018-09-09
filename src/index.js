b8d4065227e2329dab80fb95984d56a582a87363
张德志 <zhangdezhi@zhangdezhideMacBook-Air.local>
2018-09-09 22:03:46 +0800

    按装必要的依赖

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import { createStore,applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import rootReducers from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(rootReducers,
+    applyMiddleware(logger)
+
+    )
+
+ReactDOM.render(
+    <Provider store ={ store }>
+        <App/>
+    </Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/games.js
@@ -0,0 +1,9 @@
+const games = (state=[],action) => {
+    switch(action.type){
+        default:
+        return state
+
+    }
+} 
+
+export default games;
